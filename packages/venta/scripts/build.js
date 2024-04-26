48f184cf6df81c116922cd772b2842c5b458fb7a
cwooldridge1 <christian.wooldridge@ucdenver.edu>
2024-04-25 20:35:31 -0600

    added build script

---
+++ packages/venta/scripts/build.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { build } from 'vite';
+import { defineConfig } from 'vite';
+import { getBuildConfig } from '../config/utils/get-config.js';
+
+
+async function startVite() {
+  try {
+
+    await build(defineConfig(getBuildConfig('production')))
+
+  } catch (err) {
+    console.error('Failed to start the server:', err);
+    process.exit(1);
+  }
+}
+
+startVite();
