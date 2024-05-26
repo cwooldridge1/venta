c0b507521e625e39b339c96f8f147240aa2e048a
cwooldridge1 <christian.wooldridge@ucdenver.edu>
2024-05-26 15:01:49 -0600

    adjusted all tests

--- packages/babel-preset-venta/__tests__/loopedRendering.test.js
+++ packages/babel-preset-venta/__tests__/loopedRendering.test.js
@@ -19,14 +19,10 @@ test('works with list', () => {
 
     function MyComponent() {
       var items = [1, 2, 3];
-      return VentaInternal.renderVentaNode("div", null, VentaInternal.renderLoop(function () {
-        return items.map(function (item, index) {
-          return [index, function () {
-            return VentaInternal.renderVentaNode("div", {
-              key: index
-            }, item);
-          }];
-        });
+      return VentaInternal.createElement("div", null, VentaInternal.renderLoop(function (item, index) {
+        return VentaInternal.createElement("div", {
+          key: index
+        }, item);
       }, items));
     }
   `)
@@ -34,227 +30,62 @@ test('works with list', () => {
 })
 
 
-test('works with list with components', () => {
+test('works with list not in jsx context', () => {
   const code = `
-    function Row({key}) {
-      return <div>{key}</div>
-    }
     function MyComponent() {
-      const items = [1, 2, 3];
-      return (
-        <div>
-          {data.value.map((item) => {
-            return <Row key={item} />
-          })}
-        </div>
-      );
+      const arr = useState([1, 2, 3]);
+      return arr.values.map((item, index) => <div key={index}>{item}</div>)
     }
     `
-  console.log(compileCode(code))
-
   const expectedCode = normalizeCode(`
     "use strict";
 
-    function Row(_ref) {
-      var key = _ref.key;
-      return VentaInternal.renderVentaNode("div", null, key);
-    }
     function MyComponent() {
-      var items = [1, 2, 3];
-      return VentaInternal.renderVentaNode("div", null, VentaInternal.renderLoop(function () {
-        return data.value.map(function (item) {
-          return [item, function () {
-            return VentaInternal.renderVentaNode(Row, {
-              key: item
-            });
-          }];
-        });
-      }, data));
+      var arr = useState([1, 2, 3]);
+      return VentaInternal.renderLoop(function (item, index) {
+        return VentaInternal.createElement("div", {
+          key: index
+        }, item);
+      }, arr);
     }
   `)
   expect(normalizeCode(compileCode(code))).toBe(expectedCode)
 })
 
-test('works with arrow function return type', () => {
 
+test('nested loop', () => {
+  const code = `
+    function MyComponent() {
+      const items = useState([1, 2, 3]);
+      return (
+        <div>
+          {items.values.map((item, index) => (
+            <div key={index}>
+              {item.map((val, index) => (
+                <div key={val}>{val}</div>
+              ))}
+            </div>
+          ))}
+        </div>
+      );
+    }
+    `
+
+  const expectedCode = normalizeCode(`
+    "use strict";
+
+    function MyComponent() {
+      var items = useState([1, 2, 3]);
+      return VentaInternal.createElement("div", null, VentaInternal.renderLoop(function (item, index) {
+        return VentaInternal.createElement("div", {
+          key: index
+        }, VentaInternal.renderLoop(function (val, index) {
+          return VentaInternal.createElement("div", {
+            key: val
+          }, val);
+        }, item));
+      }, items));
+    }
+   `)
+  expect(normalizeCode(compileCode(code))).toBe(expectedCode)
 })
-
-
-
-// test('works with stateful list', () => {
-//   const code = `
-//     function MyComponent() {
-//       const items = useState([1, 2, 3]);
-//       return (
-//         <div>
-//           {items.values.map((item, index) => (
-//             <div key={index}>{item}</div>
-//           ))}
-//         </div>
-//       );
-//     }
-//     `
-//   const expectedCode = normalizeCode(`
-//     "use strict";
-//     function MyComponent() {
-//       var items = useState([1, 2, 3]);
-//       return VentaInternal.renderVentaNode("div", null, VentaInternal.renderLoop(function () {
-//         return items.values.map(function (item, index) {
-//           return VentaInternal.renderVentaNode("div", {
-//             key: index
-//           }, item);
-//         });
-//       }, items));
-//     }
-//   `)
-//   expect(normalizeCode(compileCode(code))).toBe(expectedCode)
-// })
-
-// test('works with list not in jsx context', () => {
-//   const code = `
-//     function MyComponent() {
-//       const arr = useState([1, 2, 3]);
-//       return arr.values.map((item, index) => <div key={index}>{item}</div>)
-//     }
-//     `
-//   const expectedCode = normalizeCode(`
-//     "use strict";
-//
-//     function MyComponent() {
-//       var arr = useState([1, 2, 3]);
-//       return VentaInternal.renderLoop(function () {
-//         return arr.values.map(function (item, index) {
-//           return VentaInternal.renderVentaNode("div", {
-//             key: index
-//           }, item);
-//         });
-//       }, arr);
-//     }
-//   `)
-//   expect(normalizeCode(compileCode(code))).toBe(expectedCode)
-// })
-//
-//
-// test('nested loop', () => {
-//   const code = `
-//     function MyComponent() {
-//       const items = useState([1, 2, 3]);
-//       return (
-//         <div>
-//           {items.values.map((item, index) => (
-//             <div key={index}>
-//               {item.map((val, index) => (
-//                 <div key={val}>{val}</div>
-//               ))}
-//             </div>
-//           ))}
-//         </div>
-//       );
-//     }
-//     `
-//   const expectedCode = normalizeCode(`
-//     "use strict";
-//
-//     function MyComponent() {
-//       var items = useState([1, 2, 3]);
-//       return VentaInternal.renderVentaNode("div", null, VentaInternal.renderLoop(function () {
-//         return items.values.map(function (item, index) {
-//           return VentaInternal.renderVentaNode("div", {
-//             key: index
-//           }, VentaInternal.renderLoop(function () {
-//             return item.map(function (val, index) {
-//               return VentaInternal.renderVentaNode("div", {
-//                 key: val
-//               }, val);
-//             });
-//           }, item));
-//         });
-//       }, items));
-//     }
-//    `)
-//   expect(normalizeCode(compileCode(code))).toBe(expectedCode)
-// })
-//
-//
-//
-// test('loop with external identifier', () => {
-//   const code = `
-//     function MyComponent() {
-//       const items = useState([1, 2, 3]);
-//       const val = 2
-//       return (
-//         <div>
-//           {items.values.map((item, index) => (
-//             <div key={index}>
-//               {val} 
-//             </div>
-//           ))}
-//         </div>
-//       );
-//     }
-//     `
-//
-//   const expectedCode = normalizeCode(`
-//     "use strict";
-//
-//     function MyComponent() {
-//       var items = useState([1, 2, 3]);
-//       var val = 2;
-//       return VentaInternal.renderVentaNode("div", null, VentaInternal.renderLoop(function () {
-//         return items.values.map(function (item, index) {
-//           return VentaInternal.renderVentaNode("div", {
-//             key: index
-//           }, val);
-//         });
-//       }, items));
-//     }
-//   `
-//   )
-//
-//   expect(normalizeCode(compileCode(code))).toBe(expectedCode)
-// })
-//
-//
-//
-// test('nested loop that uses state', () => {
-//   const code = `
-//     function MyComponent() {
-//       const items = useState([1, 2, 3]);
-//       const state = useState(2)
-//       return (
-//         <div>
-//           {items.values.map((item, index) => (
-//             <div key={index}>
-//               {item.map((val, index) => (
-//                 <div key={val}>{state.value}</div>
-//               ))}
-//             </div>
-//           ))}
-//         </div>
-//       );
-//     }
-//     `
-//   const expectedCode = normalizeCode(`
-//       "use strict";
-//
-//       function MyComponent() {
-//         var items = useState([1, 2, 3]);
-//         var state = useState(2);
-//         return VentaInternal.renderVentaNode("div", null, VentaInternal.renderLoop(function () {
-//           return items.values.map(function (item, index) {
-//             return VentaInternal.renderVentaNode("div", {
-//               key: index
-//             }, VentaInternal.renderLoop(function () {
-//               return item.map(function (val, index) {
-//                 return VentaInternal.renderVentaNode("div", {
-//                   key: val
-//                 }, state.value);
-//               });
-//             }, item));
-//           });
-//         }, items));
-//       }
-//    `)
-//
-//   expect(normalizeCode(compileCode(code))).toBe(expectedCode)
-// })
